19b70be9ea28fb3693eb4fe32531b28ffb5bd92c
Zvezdari <3314227204@qq.com>
2026-06-02 13:32:59 +0800

    normal update

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -99,6 +99,7 @@ return {
             get_cwd = function (_)
               return vim.fn.getcwd()
             end,
+            show_hidden_files_by_default = true,
           },
         },
         snippets = {
